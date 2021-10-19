d374e7f714abe8fbb2bb74d56a7eea325dd9834b
Euromancer <euromancer@icloud.com>
2021-10-19 19:21:28 +0300

    Linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
   ],
   'rules': {
     'no-console': 2,
-    'indent': ['error', 4],
+    'indent': ['error', 2],
     'linebreak-style': ['error', 'unix'],
     'quotes': ['error', 'single'],
     'semi': ['error', 'always'],
